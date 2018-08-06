8d8cf9fdf9fa601a42b71e19538899bf8c414ae4
Raghav <raghavb293@gmail.com>
2018-08-06 20:54:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/velmurugan/retrofitandroidexample/ApiInterface.java
@@ -0,0 +1,12 @@
+package com.example.velmurugan.retrofitandroidexample;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiInterface
+{
+    @GET("volley_array.json")
+    Call<List<Movie>> getMovies();
+}
